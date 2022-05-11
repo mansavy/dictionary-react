3319d0f8dafde9d05ec55a97b68732fb9b72b9af
mansavy <100310641+mansavy@users.noreply.github.com>
2022-05-11 15:10:56 -0300

    Created search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
-  return <div className="App">HELLLO</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Mandy Savoie</small>
+        </footer>
+      </div>
+    </div>
+  );
 }
